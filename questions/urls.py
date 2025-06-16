90a77a01554596448f854ffd41a150e3040f252a
gayratovi <gayratov2024@gmail.com>
2025-06-16 22:05:58 +0100

    Complete admin question management: list, edit, delete

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import add_question_view, question_list_view
+from .views import add_question_view, question_list_view, edit_question_view, delete_question_view
 
 urlpatterns = [
     path('add/', add_question_view, name='add_question'),
     path('list/', question_list_view, name='question_list'),
+    path('edit/<int:question_id>/', edit_question_view, name='edit_question'),
+    path('delete/<int:question_id>/', delete_question_view, name='delete_question'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import QuestionForm
 from .models import Question
+from django.shortcuts import get_object_or_404
+
+@login_required
+def edit_question_view(request, question_id):
+    if request.user.role != 'admin':
+        return redirect('login')
+
+    question = get_object_or_404(Question, id=question_id)
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return redirect('question_list')
+    else:
+        form = QuestionForm(instance=question)
+
+    return render(request, 'questions/edit_question.html', {'form': form, 'question': question})
 
 @login_required
 def question_list_view(request):
@@ -26,3 +44,16 @@ def add_question_view(request):
         form = QuestionForm()
 
     return render(request, 'questions/add_question.html', {'form': form})
+
+@login_required
+def delete_question_view(request, question_id):
+    if request.user.role != 'admin':
+        return redirect('login')
+
+    question = get_object_or_404(Question, id=question_id)
+
+    if request.method == 'POST':
+        question.delete()
+        return redirect('question_list')
+
+    return render(request, 'questions/confirm_delete.html', {'question': question})
