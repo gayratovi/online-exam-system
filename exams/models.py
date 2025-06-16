a9983b64d9717e9c498cc99138e5837290e029cd
gayratovi <gayratov2024@gmail.com>
2025-06-16 22:10:52 +0100

    Add Exam and ExamQuestion models for exam creation

--- exams/models.py
+++ exams/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from questions.models import Question
 
-# Create your models here.
+class Exam(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class ExamQuestion(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.exam.title} - {self.question.question_text[:50]}"
