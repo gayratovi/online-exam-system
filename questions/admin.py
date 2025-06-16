78cb14b0287da99d52a717d4032fde0d8d88af08
gayratovi <gayratov2024@gmail.com>
2025-06-16 15:40:39 +0100

    Register Question model in Django admin for testing

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'question_type', 'correct_answer')
+    list_filter = ('question_type',)
+    search_fields = ('question_text',)
